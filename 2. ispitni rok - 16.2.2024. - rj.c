5777718e66978ed6e8bdae7bf25ae930f1b803d9
iglibic <ivanglibic77@gmail.com>
2025-02-08 18:52:30 +0100

    Update 2. ispitni rok - 16.2.2024. - rj.c

--- 2. ispitni rok - 16.2.2024. - rj.c
+++ 2. ispitni rok - 16.2.2024. - rj.c
@@ -1,320 +1,186 @@
-#define _CRT_SECURE_NO_WARNINGS
-#define MAX_ID_LEN (16)
-#define MAX_ID (8)
-#define MAX_NAME_LEN (64)
-#define MAX_NAME_LENGTH (32)
+#include <stdio.h>
+#include <stdlib.h>
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<time.h>
+struct _date;
+typedef struct _date* DateP;
+typedef struct _date {
+    int day;
+    int month;
+    int year;
+} Date;
 
+struct _temperature;
+typedef struct _temperature* TemperatureP;
+typedef struct _temperature {
+    int depth;
+    int temperature;
+    DateP dateOfMeasurments;
+    TemperatureP next;
+};
 
-//podatke o clanovima knjiznice trebaju se spremiti u jednostruku vezanu listu sortiranu po prezimenu, pa po imenu pa po id-u
-//podatke o posudenim knjigama trebaju se spremiti u jednostruko vezanu listu sortiranu po nazivu knjige
-//ocjdena 2 pronaci clana knjiznice koji je pročitao najviše knjiga te ispisati njegovo ime i prezime
-//ocjena 3 clana izbrisati iz vezane liste
+// Funkcije koje se koriste u programu
+int ReadFile(TemperatureP head, char* filename);
+int MakeList(TemperatureP head, int dubina, int temp, int dan, int mjesec, int godina);
+TemperatureP CreateT(int dubina, int temp, int dan, int mjesec, int godina);
+int PrintList(TemperatureP head);
+float ProsjecnaTemperatura(TemperatureP head);
+int DeleteTemp(TemperatureP head, float prosjecna);
+void FreeList(TemperatureP head);
 
-struct _member;
-typedef struct _member* MemberP;
-typedef struct _member {
-	char ID[MAX_ID_LEN];
-	char firstname[MAX_NAME_LEN];
-	char lastName[MAX_NAME_LEN];
-	MemberP next;
-}Member;
-
-struct _book;
-typedef struct _book* BookP;
-typedef struct _book {
-	char name[MAX_NAME_LEN];
-	char memberID[MAX_ID_LEN];
-	BookP next;
-}Book;
-
-int ReadFile(MemberP head, char* filename);
-int MakeList(MemberP head, char* name, char* surname, char* ID);
-MemberP CreateNewMember(char* name, char* surname, char* ID);
-BookP CreateNewBook(char* name, char* memberId);
-int ReadFile2(BookP head2, char* filename);
-int MakeListOfBooks(BookP head2, char* nameBook, char* memId);
-int SortName(MemberP head, MemberP new);
-int Printmem(MemberP head);
-int printBook(BookP head2);
-BookP findID(BookP head, char* id);
-MemberP FindTheMemberWithMostBooks(MemberP head, BookP head2);
 int main() {
+    Temperature head = { .depth = 0, .dateOfMeasurments = NULL, .next = NULL };
+    float prosjecnaTemp;
 
-	Member head = {
-		.ID = {0},
-		.firstname = {0},
-		.lastName = {0},
-		.next = NULL
-	};
-	Book head2 = {
-		.name = {0},
-		.memberID = {0},
-		.next = NULL,
-	};
-	char id[MAX_ID_LEN] = { 0 };
-	printf("list of members:\n");
-	ReadFile(&head, "clanovi.txt");
-	Printmem(head.next);
+    // Čitanje podataka iz datoteke
+    ReadFile(&head, "temperature.txt");
+    
+    // Ispisivanje originalne liste
+    printf("Originalna lista:\n");
+    PrintList(head.next);
 
-	printf("\n");
-	printf("List of books:\n");
-	ReadFile2(&head2, "knjige.txt");
-	printBook(head2.next);
-	//FindTheMemberWithMostBooks(&head, &head2);
+    // Izračun prosječne temperature i brisanje mjerenja manjih od prosječne temperature
+    prosjecnaTemp = ProsjecnaTemperatura(head.next);
+    printf("\nProsjecna temperatura u 2023. godini i na dubini >50 m je %.2f\n", prosjecnaTemp);
+    DeleteTemp(&head, prosjecnaTemp);
+    
+    // Ispisivanje liste nakon brisanja
+    printf("\nLista nakon brisanja mjerenja manjih od prosječne temperature:\n");
+    PrintList(head.next);
 
-	DeleteMember(&head, &head2);
-	Printmem(head.next);
-	printBook(head2.next);
-	/*DEleteMembers(&head);
-	DEleteBooks(&head2);
-	printBook(head2.next);
-	Printmem(head.next);*/
+    // Oslobađanje memorije
+    FreeList(head.next);
 
-	return 0;
-
-}
-int DEleteBooks(BookP head) {
-	BookP temp = NULL;
-	while (head->next != NULL) {
-		temp = head->next;
-		head->next = temp->next;
-		free(temp);
-	}
-	return EXIT_SUCCESS;
-
-}
-int DEleteMembers(MemberP head1) {
-	MemberP temp = NULL;
-	while (head1->next != NULL) {
-		temp = head1->next;
-		head1->next = temp->next;
-		free(temp);
-	}
-	return EXIT_SUCCESS;
-
+    return 0;
 }
 
-MemberP  FindTheMemberWithMostBooks(MemberP head, BookP head2) {
-	int maxbooks = 0;
-	MemberP maxClan = NULL;
+int ReadFile(TemperatureP head, char* filename) {
+    FILE* fptr = fopen(filename, "r");
+    if (!fptr) {
+        printf("File couldn't be opened\n");
+        return EXIT_FAILURE;
+    }
 
-	MemberP current = head;
-	while (current!=NULL) {
-		int bookcount = 0;
-		BookP currentB = head2;
-		while (currentB!=NULL) {
-			if (strcmp(currentB->memberID, current->ID) == 0) {
-				bookcount++;
-			}
-			currentB = currentB->next;
-		}
-		if (bookcount > maxbooks) {
-			maxbooks = bookcount;
-			maxClan = current;
-		}
-		current = current->next;
-	}
+    int dan, mjesec, godina, dubina, temp;
+    while (fscanf(fptr, "%d.%d.%d %d %d", &dan, &mjesec, &godina, &dubina, &temp) == 5) {
+        MakeList(head, dubina, temp, dan, mjesec, godina);
+    }
 
-
-	printf("Clan koji je procitao najvise knjiga je %s %s (%s). Pročitao je %d knjiga.\n", maxClan->firstname, maxClan->lastName, maxClan->ID,maxbooks);
-
-	return maxClan;
-}
-int ReadFile2(BookP head2, char* filename) {
-	char name[MAX_NAME_LEN] = { 0 };
-	char memberId[MAX_ID_LEN] = { 0 };
-	FILE* fptr = NULL;
-
-	fptr = fopen(filename, "r");
-	if (!fptr) {
-		printf("Error while opening file!\n");
-		return EXIT_FAILURE;
-	}
-	else {
-		printf("File opened!\n");
-	}
-
-	while (!feof(fptr)) {
-		fscanf(fptr, "%s %s", memberId, name);
-
-		MakeListOfBooks(head2, name, memberId);
-
-	}
-	fclose(fptr);
-	return EXIT_SUCCESS;
-
-
-}
-int printBook(BookP head2) {
-	if (head2 == NULL) {
-		printf("list is empty!\n");
-	}
-
-
-	while (head2 != NULL) {
-		printf("%s %s\n", head2->name, head2->memberID);
-		head2 = head2->next;
-	}
-	return EXIT_SUCCESS;
-}
-int MakeListOfBooks(BookP head2, char* nameBook, char* memId) {
-	BookP newb = NULL;
-	newb = CreateNewBook(nameBook, memId);
-	while (head2->next != NULL && strcmp(head2->next->name, newb->name) < 0) {
-		head2 = head2->next;
-	}
-	newb->next = head2->next;
-	head2->next = newb;
-
-	return EXIT_SUCCESS;
+    fclose(fptr);
+    return EXIT_SUCCESS;
 }
 
-BookP CreateNewBook(char* name, char* memberId) {
-	BookP newBook = NULL;
-	newBook = (BookP)malloc(sizeof(Book));
+int MakeList(TemperatureP head, int dubina, int temp, int dan, int mjesec, int godina) {
+    TemperatureP newTemp = CreateT(dubina, temp, dan, mjesec, godina);
+    while (head->next != NULL && DateCompare(head->next, newTemp) < 0) {
+        head = head->next;
+    }
 
-	if (!newBook) {
-		printf("Allocation failed!\n");
-	}
-
-	strcpy(newBook->name, name);
-	strcpy(newBook->memberID, memberId);
-	newBook->next = NULL;
-
-	return newBook;
-}
-int ReadFile(MemberP head, char* filename) {
-	char ID[MAX_ID_LEN] = { 0 };
-	char surname[MAX_NAME_LEN] = { 0 };
-	char name[MAX_NAME_LEN] = { 0 };
-	FILE* fptr = NULL;
-
-	fptr = fopen(filename, "r");
-	if (!fptr) {
-		printf("Error while opening file!\n");
-		return EXIT_FAILURE;
-	}
-	else {
-		printf("File opened!\n");
-	}
-
-	while (!feof(fptr)) {
-		fscanf(fptr, "%s %s %s", ID, surname, name);
-		MakeList(head, name, surname, ID);
-
-
-	}
-	fclose(fptr);
-
-	return EXIT_SUCCESS;
-
-}
-int MakeList(MemberP head, char* name, char* surname, char* ID) {
-	MemberP new = NULL;
-
-	new = CreateNewMember(name, surname, ID);
-	while (head->next != NULL && SortName(head->next, new) < 0) {
-		head = head->next;
-	}
-	new->next = head->next;
-	head->next = new;
-
-	return EXIT_SUCCESS;
-}
-int SortName(MemberP head, MemberP new) {
-	if (strcmp(head->lastName, new->lastName) > 0) {
-		return 1;
-	}
-	else if (strcmp(head->lastName, new->lastName) < 0) {
-		return -1;
-	}
-	else if (strcmp(head->lastName, new->lastName) == 0) {
-		if (strcmp(head->firstname, new->firstname) > 0) {
-			return 1;
-		}
-		else if (strcmp(head->firstname, new->firstname) < 0) {
-			return -1;
-		}
-		else if (strcmp(head->firstname, new->firstname) == 0) {
-			if (strcmp(head->ID, new->ID) > 0) {
-				return 1;
-			}
-			else if (strcmp(head->ID, new->ID) < 0) {
-				return -1;
-			}
-		}
-
-	}
-
-}
-MemberP CreateNewMember(char* name, char* surname, char* ID) {
-	MemberP newMem = NULL;
-	newMem = (MemberP)malloc(sizeof(Member));
-	if (!newMem) {
-		printf("Allocation failed!\n");
-		return NULL;
-	}
-	strcpy(newMem->firstname, name);
-	strcpy(newMem->lastName, surname);
-	strcpy(newMem->ID, ID);
-	newMem->next = NULL;
-
-	return newMem;
-}
-int Printmem(MemberP head) {
-
-	if (head == NULL) {
-		printf("list is empty!\n");
-	}
-
-
-	while (head != NULL) {
-		printf("%s %s %s\n", head->firstname, head->lastName, head->ID);
-		head = head->next;
-	}
-	return EXIT_SUCCESS;
+    newTemp->next = head->next;
+    head->next = newTemp;
+    return EXIT_SUCCESS;
 }
 
-int DeleteMember(MemberP head, BookP head2){
-	MemberP maxhead = NULL;
-	maxhead=FindTheMemberWithMostBooks(head, head2);
-	MemberP prevM = head;
-	MemberP currentM = NULL;
-	if (maxhead == NULL) {
-		printf("No members found!\n");
-	}
-	//delete the books
-	BookP prevB = head2;
-	
-	while (prevB->next != NULL) {
-		BookP currentB = prevB->next;
-		if (strcmp(currentB->memberID, maxhead->ID) == 0) {
-			prevB->next = currentB->next;
-			free(currentB);
-		}
-		else {
-			prevB = currentB;
-			
-		}
-	}
+TemperatureP CreateT(int dubina, int temp, int dan, int mjesec, int godina) {
+    TemperatureP newTemp = (TemperatureP)malloc(sizeof(Temperature));
+    if (!newTemp) {
+        printf("Error in memory allocation!\n");
+        return NULL;
+    }
 
-	while (prevM->next != NULL) {
-		currentM = prevM->next;
-		if (strcmp(currentM->ID, maxhead->ID) == 0) {
-			
-			prevM->next = currentM->next;
-			free(currentM);
-			
-		}
-		else {
-			prevM = currentM;
-			
-		}
-	}
-	return EXIT_SUCCESS;
+    newTemp->depth = dubina;
+    newTemp->next = NULL;
+    newTemp->temperature = temp;
+
+    newTemp->dateOfMeasurments = (DateP)malloc(sizeof(Date));
+    if (!(newTemp->dateOfMeasurments)) {
+        printf("Error in memory allocation!\n");
+        free(newTemp);
+        return NULL;
+    }
+
+    newTemp->dateOfMeasurments->day = dan;
+    newTemp->dateOfMeasurments->month = mjesec;
+    newTemp->dateOfMeasurments->year = godina;
+    return newTemp;
+}
+
+int DateCompare(TemperatureP head, TemperatureP head2) {
+    int rez = head->dateOfMeasurments->year - head2->dateOfMeasurments->year;
+    if (rez == 0) {
+        rez = head->dateOfMeasurments->month - head2->dateOfMeasurments->month;
+        if (rez == 0) {
+            rez = head->dateOfMeasurments->day - head2->dateOfMeasurments->day;
+        }
+    }
+    return rez;
+}
+
+int PrintList(TemperatureP head) {
+    if (head == NULL) {
+        printf("List is empty!\n");
+        return EXIT_SUCCESS;
+    }
+
+    while (head != NULL) {
+        printf("%d.%d.%d. Depth: %d, Temperature: %d\n",
+               head->dateOfMeasurments->day, head->dateOfMeasurments->month,
+               head->dateOfMeasurments->year, head->depth, head->temperature);
+        head = head->next;
+    }
+    return EXIT_SUCCESS;
+}
+
+float ProsjecnaTEmperatura(TemperatureP head) {
+    int count = 0;
+    float sum = 0.0;
+    float prosj_temp = 0.0;
+
+    while (head != NULL) {
+        if (head->dateOfMeasurment->year == 2023 && head->depth > 50) {
+            sum += head->temperature;
+            count++;
+        }
+        head = head->next;
+    }
+
+    if (count > 0) {
+        prosj_temp = sum / count;
+    } else {
+        prosj_temp = 0.0;
+    }
+
+    return prosj_temp;
+}
+
+
+int DeleteTemp(TemperatureP head, float prosjecna) {
+    TemperatureP temp = NULL;
+    TemperatureP current = head->next;
+    TemperatureP prev = head;
+
+    while (current != NULL) {
+        if (current->temperature < prosjecna) {
+            temp = current;
+            prev->next = current->next;
+            free(temp->dateOfMeasurments);
+            free(temp);
+            current = prev->next;
+        } else {
+            prev = current;
+            current = current->next;
+        }
+    }
+    return EXIT_SUCCESS;
+}
+
+void FreeList(TemperatureP head) {
+    TemperatureP current = head;
+    TemperatureP next;
+
+    while (current != NULL) {
+        next = current->next;
+        free(current->dateOfMeasurments);
+        free(current);
+        current = next;
+    }
 }
